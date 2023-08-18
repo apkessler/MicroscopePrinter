c7a913d25ef7c75b72555e73ccfbde758af50b90
Andrew P. Kessler <andrewkessler6@gmail.com>
2023-08-17 20:15:52 -0700

    Added script to revert to normal wifi

---
+++ scripts/revert_to_normal_wifi.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# This script will put the Pi back into a mode where it can connect to a WiFi
+# network as a normal client (after a reboot).
+
+#Confirm actually being run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'This script must be run as root! Did you forget sudo?' >&2
+    exit 1
+fi
+
+set -xeo
+
+systemctl disable hostapd dnsmasq
+cp /etc/dhcpcd.conf.orig /etc/dhcpcd.conf
+
+echo "Please enter name (SSID) of WiFi network you want to connect to: "
+read ssid
+echo "Ok, will look for SSID $ssid."
+
+echo "Please enter password for WiFi network ($ssid): "
+read password
+echo "OK, the password will be $password."
+
+#Create a wpa_supplicant.conf...
+cat <<EOF >>/etc/wpa_supplicant/wpa_supplicant.conf
+country=US
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+network={
+ ssid="$ssid"
+ scan_ssid=1
+ psk="$password"
+ key_mgmt=WPA-PSK
+}
+EOF
+
+echo "Rebooting in 5sec. If you screwed up, press Ctrl-c and retry. Otherwise, wait for reboot."
+sleep 5
+reboot
